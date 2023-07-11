2570a071dbd05d32862604d1ae769efad48ec0b9
Inhu Jo <inhudev@gmail.com>
2023-07-11 19:07:29 +0900

    feat : index.html 타이틀 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,8 +7,9 @@ const client = new Octokit({
   auth: import.meta.env.VITE_GITHUB_TOKEN,
 });
 
-export const fetchIssueList = (owner = OWNER, repo = REPO) => {
+export const fetchIssueList = (page = 1, owner = OWNER, repo = REPO) => {
   return client.request(`GET /repos/${owner}/${repo}/issues`, {
+    page: page,
     per_page: 50,
     headers: {
       'X-GitHub-Api-Version': '2022-11-28',
