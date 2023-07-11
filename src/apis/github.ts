766cab737fcfa5934ce19a09ab68cc4ffb79b64b
kimseunggyu <dev.seunggyu@gmail.com>
2023-07-11 22:14:59 +0900

    refactor : getIssues API 수정

--- src/apis/github.ts
+++ src/apis/github.ts
@@ -3,11 +3,14 @@ import { components } from '@octokit/openapi-types';
 
 import { client } from '@/apis';
 
-// https://api.github.com/repos/facebook/react/issues?sort=comments&per_page=5&page=2
-type Issue = components['schemas']['issue'];
-export async function getIssues(sort?: string): Promise<Issue[]> {
-  const query = qs.stringify({ sort: sort || 'comments', per_page: 5, page: 1 });
-  // const query = qs.stringify({ sort: undefined, per_page: 5, page: 1 });
+export type Issue = components['schemas']['issue'];
+
+/**
+ * @link https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#list-repository-issues
+ * @param sort
+ */
+export async function getIssues(sort = 'comments'): Promise<Issue[]> {
+  const query = qs.stringify({ sort: sort, per_page: 5, page: 1 });
   const response = await client.get(`/facebook/react/issues?${query}`);
   return response.data;
 }
