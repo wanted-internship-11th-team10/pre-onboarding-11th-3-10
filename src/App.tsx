da510568ebc40799ebac3cf46e98c460742744bd
kimseunggyu <dev.seunggyu@gmail.com>
2023-07-11 18:09:01 +0900

    feat : vite 환경 변수 설정 예제 추가 (.env)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+
+console.log('VITE .env 접근 테스트 :: GITHUB_TOKEN : ', GITHUB_TOKEN);
 function App() {
-  return <>React Vite</>;
+  return <>React Vite: {GITHUB_TOKEN}</>;
 }
 
 export default App;
---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+interface ImportMetaEnv {
+  readonly VITE_GITHUB_TOKEN: string;
+}
