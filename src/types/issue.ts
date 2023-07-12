4230072cbb3f8c46d7799452157aa7c69a96b895
sik9252 <sik9252@naver.com>
2023-07-12 18:03:46 +0900

    fix: 모든곳에서 공통으로 쓰일 issue type만 놔두고 모두 제거

--- src/types/issue.ts
+++ src/types/issue.ts
@@ -1,27 +1,14 @@
-import { PropsWithChildren } from 'react';
-
 export interface Issue {
   id: number;
-  state: string; // 이슈 상태
-  number: number; // 이슈 번호
-  title: string; // 이슈 제목
-  user: { login: string; avatar_url: string }; // 작성자, 작성자 프로필 이미지
-  created_at: string; // 작성일
-  comments: number; // 코멘트 수
-  body: string; // 본문
-  onClick: () => void;
-}
-
-export interface IssueType {
-  issues: Issue[];
-  id: number;
-  state: string; // 이슈 상태
-  number: number; // 이슈 번호
-  title: string; // 이슈 제목
-  user: { login: string; avatar_url: string }; // 작성자, 작성자 프로필 이미지
-  created_at: string; // 작성일
-  comments: number; // 코멘트 수
-  body: string; // 본문
-  repository_url: string; // 저장소 url
-  children: PropsWithChildren;
+  state: string;
+  number: number;
+  title: string;
+  user: {
+    login: string;
+    avatar_url: string;
+  };
+  created_at: string;
+  comments: number;
+  body: string;
+  repository_url: string;
 }
