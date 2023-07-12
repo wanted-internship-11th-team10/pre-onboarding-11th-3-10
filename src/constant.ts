099a5438f28b5f679de6af34e087fb8d7458c24f
Honey <yurjune@naver.com>
2023-07-12 19:33:41 +0900

    chore: IMAGE_URL env 추가

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,5 @@
 export const env = {
   ACCESS_TOKEN: import.meta.env.VITE_ACCESS_TOKEN ?? '',
   BASE_URL: import.meta.env.VITE_BASE_URL ?? '',
+  IMAGE_URL: import.meta.env.VITE_IMAGE_URL ?? '',
 };
