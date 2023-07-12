a906a4a2bc84eb25d53bbb881dd451310a96e331
Honey <yurjune@naver.com>
2023-07-12 18:57:19 +0900

    feat: getIssues api 생성

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import type { AxiosResponse } from 'axios';
+import axios from 'axios';
+
+import { env } from '@/constant';
+
+const client = axios.create({
+  baseURL: env.BASE_URL,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28',
+    Accept: 'application/vnd.github+json',
+    Authorization: `Bearer ${env.ACCESS_TOKEN}`,
+  },
+});
+const responseBody = (response: AxiosResponse) => response.data;
+
+type getIssusesRequest = {
+  user: string;
+  repo: string;
+};
+export const getIssues = ({ user, repo }: getIssusesRequest) =>
+  client.get(`/repos/${user}/${repo}/issues?state=open`).then(responseBody);
---
+++ src/constant.ts
@@ -0,0 +1,4 @@
+export const env = {
+  ACCESS_TOKEN: import.meta.env.VITE_ACCESS_TOKEN ?? '',
+  BASE_URL: import.meta.env.VITE_BASE_URL ?? '',
+};
