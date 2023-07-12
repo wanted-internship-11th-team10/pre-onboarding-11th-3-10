6e7369f1cab1e640ba9c8c33ed97b531fee3f822
jhwmac <jkr2505@naver.com>
2023-07-12 20:46:41 +0900

    style : Header style 적용

--- src/layout/HeaderLayout.tsx
+++ src/layout/HeaderLayout.tsx
@@ -7,11 +7,14 @@ interface ComponentProps {
 const HeaderLayout = ({ children }: ComponentProps) => {
   return (
     <Layout>
-      <Header>Facebook React</Header>
+      <Header>
+        <b>Facebook React</b>
+      </Header>
       {children}
     </Layout>
   );
 };
+
 const Layout = styled.div`
   display: flex;
   margin: 0 auto;
@@ -20,7 +23,26 @@ const Layout = styled.div`
   padding: 0 10%;
   max-width: 1280px;
 `;
-const Header = styled.h1`
+const Header = styled.div`
+  position: relative;
+  display: block;
   text-align: center;
+  margin: 50px 0;
+  b {
+    position: relative;
+    font-size: 40px;
+    &::before {
+      content: '';
+      position: absolute;
+      display: block;
+      left: 50%;
+      transform: translateX(-50%);
+      bottom: 10px;
+      width: 100%;
+      height: 10px;
+      background-color: #cad2fb;
+      z-index: -1;
+    }
+  }
 `;
 export default HeaderLayout;
