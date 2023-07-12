bee6966b0fd1df0737f68dc9f09850cb34732b2d
sik9252 <sik9252@naver.com>
2023-07-12 09:12:49 +0900

    feat: 공통 Header 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,29 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { IssueContext } from '@/context/IssueContext';
+
 function Header() {
-  return <div>Header</div>;
+  const issue = useContext(IssueContext);
+  const [header, setHeader] = useState({
+    organizationName: '',
+    repositoryName: '',
+  });
+
+  useEffect(() => {
+    if (issue && issue[0] && issue?.[0].repository_url) {
+      const pathParts = issue?.[0].repository_url.split('/');
+      setHeader({
+        organizationName: pathParts[4],
+        repositoryName: pathParts[5],
+      });
+    }
+  }, [issue]);
+
+  return (
+    <div>
+      {header.organizationName} / {header.repositoryName}
+    </div>
+  );
 }
 
 export default Header;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Header from '@/components/Header';
 import IssueDetail from '../pages/IssueDetail';
 import IssueList from '../pages/IssueList';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<IssueList />} />
         <Route path="/:id" element={<IssueDetail />} />
