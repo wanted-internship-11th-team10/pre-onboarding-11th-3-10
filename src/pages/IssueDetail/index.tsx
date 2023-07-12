39bb22a39e87a28c8b2db96575413b073891a0e0
sik9252 <sik9252@naver.com>
2023-07-12 10:25:59 +0900

    feat: 이슈 상세글 불러오는 기능 추가

--- src/pages/IssueDetail/index.tsx
+++ src/pages/IssueDetail/index.tsx
@@ -1,5 +1,37 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { IssueContext } from '@/context/IssueContext';
+import { IssueType } from '@/types/issue';
+
 function IssueDetail() {
-  return <div>IssueDetail</div>;
+  const { id } = useParams();
+  const issue = useContext(IssueContext);
+  const [selectedIssue, setSelectedIssue] = useState<IssueType>();
+
+  useEffect(() => {
+    if (issue && id) {
+      const selectedIssue = issue?.find((issue) => issue.number === parseInt(id));
+      setSelectedIssue(selectedIssue);
+    }
+  }, [issue, id]);
+
+  if (!selectedIssue) {
+    return <div>게시글을 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <>
+      <div>이슈번호 {selectedIssue.number}</div>
+      <div>이슈제목 {selectedIssue.title}</div>
+      <div>작성자 {selectedIssue.user.login}</div>
+      <div>작성일 {selectedIssue.created_at}</div>
+      <div>코멘트 수 {selectedIssue.comments}</div>
+      <div>작성자 프로필 이미지 {selectedIssue.user.avatar_url}</div>
+      {/* 본문은 마크다운 뷰어 사용 */}
+      <div>본문 {selectedIssue.body}</div>
+    </>
+  );
 }
 
 export default IssueDetail;
