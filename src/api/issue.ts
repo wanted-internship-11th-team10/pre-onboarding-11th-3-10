da1d6e995665b0f97c4cdb3a4b6b9b1f05b39828
sik9252 <sik9252@naver.com>
2023-07-12 23:30:03 +0900

    fix: 이슈 상세 정보 api 요청해서 가져오도록 수정

--- src/api/issue.ts
+++ src/api/issue.ts
@@ -6,3 +6,8 @@ export async function getIssue(): Promise<Issue[]> {
   const response = await client.get('/issues?sort=comments&direction=desc&per_page=10');
   return response.data;
 }
+
+export async function getIssueDetail(issue_number: number): Promise<Issue> {
+  const response = await client.get(`/issues/${issue_number}`);
+  return response.data;
+}
--- src/pages/IssueDetail/index.tsx
+++ src/pages/IssueDetail/index.tsx
@@ -1,28 +1,31 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { getIssueDetail } from '@/api/issue';
 import Error from '@/components/Error';
 import IssueMarkDownViewer from '@/components/IssueMarkDownViewer';
-import { IssueContext } from '@/context/IssueContext';
+import Loader from '@/components/Loader';
 import { Issue } from '@/types/issue';
 import { BottomBox, IssueContainer, IssueTitleContainer, LeftBox, ProfileImgBox, RightBox, TopBox } from './style';
 
 function IssueDetail() {
   const { id } = useParams();
-  const { issues } = useContext(IssueContext);
   const [selectedIssue, setSelectedIssue] = useState<Issue>();
-  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    if (issues && id) {
-      const selectedIssue = issues?.find((issue) => issue.number === parseInt(id));
-      if (!selectedIssue) {
-        setError(true);
-      } else {
-        setSelectedIssue(selectedIssue);
-      }
+    if (id) {
+      getIssueDetail(parseInt(id))
+        .then((res) => {
+          setSelectedIssue(res);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error.response.data.message);
+        });
     }
-  }, [issues, id]);
+  }, [id]);
 
   if (error) {
     return <Error message={'게시글이 없습니다.'} />;
@@ -30,27 +33,33 @@ function IssueDetail() {
 
   return (
     <IssueContainer>
-      {selectedIssue && (
+      {loading ? (
+        <Loader />
+      ) : (
         <>
-          <IssueTitleContainer>
-            <ProfileImgBox>
-              <img src={selectedIssue.user.avatar_url} alt="프로필" width="50" height="50" />
-            </ProfileImgBox>
-            <div>
-              <LeftBox>
-                <TopBox>
-                  <div>#{selectedIssue.number}</div>
-                  <div>{selectedIssue.title}</div>
-                </TopBox>
-                <BottomBox>
-                  <div>작성자: {selectedIssue.user.login},</div>
-                  <div>작성일: {selectedIssue.created_at.slice(0, 10)}</div>
-                </BottomBox>
-              </LeftBox>
-              <RightBox>코멘트 {selectedIssue.comments}</RightBox>
-            </div>
-          </IssueTitleContainer>
-          <IssueMarkDownViewer content={selectedIssue.body} />
+          {selectedIssue && (
+            <>
+              <IssueTitleContainer>
+                <ProfileImgBox>
+                  <img src={selectedIssue.user.avatar_url} alt="프로필" width="50" height="50" />
+                </ProfileImgBox>
+                <div>
+                  <LeftBox>
+                    <TopBox>
+                      <div>#{selectedIssue.number}</div>
+                      <div>{selectedIssue.title}</div>
+                    </TopBox>
+                    <BottomBox>
+                      <div>작성자: {selectedIssue.user.login},</div>
+                      <div>작성일: {selectedIssue.created_at.slice(0, 10)}</div>
+                    </BottomBox>
+                  </LeftBox>
+                  <RightBox>코멘트 {selectedIssue.comments}</RightBox>
+                </div>
+              </IssueTitleContainer>
+              <IssueMarkDownViewer content={selectedIssue.body} />
+            </>
+          )}
         </>
       )}
     </IssueContainer>
