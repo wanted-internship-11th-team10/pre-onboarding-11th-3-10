56715526537401b09c6210165f59418e6439f997
Inhu Jo <inhudev@gmail.com>
2023-07-13 15:19:24 +0900

    feat : cache 추가

--- src/hook/useFetchIssue.tsx
+++ src/hook/useFetchIssue.tsx
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 
 import { fetchIssue, IssueData } from '@/api/issue';
 import { wrapPromise } from '@/utils';
+import { LocalCache } from '@/utils/cache';
+
+const cache = new LocalCache();
 
 export function useFetchIssue() {
   const [issue, setIssue] = useState<IssueData | Promise<IssueData>>();
@@ -10,10 +13,18 @@ export function useFetchIssue() {
 
   useEffect(() => {
     if (!number) return;
+    if (cache.has(number)) return;
 
     const promise = fetchIssue(number);
     setIssue(wrapPromise(promise));
   }, [number]);
 
-  return issue;
+  // cache
+  useEffect(() => {
+    if (!issue) return;
+    if (cache.has(number + '')) return;
+    cache.set(number + '', issue as IssueData);
+  }, [number, issue]);
+
+  return number ? cache.get(number) || issue : null;
 }
